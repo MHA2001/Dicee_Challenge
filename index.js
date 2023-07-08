8771cfb8c86d0d84f264838f390b384bf03fcd11
MHA2001 <mha.akhoundi@gmail.com>
2023-07-08 07:55:01 +0330

    END

--- index.js
+++ index.js
@@ -5,16 +5,31 @@ function refresh(){
     document.querySelector(".img1").setAttribute("src" , "./images/dice"+player1+".png");
     document.querySelector(".img2").setAttribute("src" , "./images/dice"+player2+".png");
     if (player1 == player2){
-        document.getElementsByTagName("h1")[0].innerText = "Draw !"
+        document.getElementsByTagName("h1")[0].innerText = "Draw !";
 
     }
     else if (player1>player2){
-        document.getElementsByTagName("h1")[0].innerText = "player 1 Wins !"
+        document.getElementsByTagName("h1")[0].innerText = "player 1 Wins !";
+        document.querySelector("h1").style.setProperty('--show1','inline-block');
+        document.querySelector("h1").style.setProperty('--show2','none');
+     
+    }   
+    else{
+        document.getElementsByTagName("h1")[0].innerText = "player 2 Wins !";
+        document.querySelector("h1").style.setProperty('--show2','inline-block');
+        document.querySelector("h1").style.setProperty('--show1','none');
+
 
     }
-    else{
-        document.getElementsByTagName("h1")[0].innerText = "player 2 Wins !"
-    }
 }
+
 refresh();
+
+// window.onload = (event) => {
+//     var x =sessionStorage.getItem("reloading");
+//     if(x){
+//         sessionStorage.removeItem("reloading");
+//         refresh();
+//     }
+// }
 
